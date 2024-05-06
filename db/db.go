dc64ea874182d7c5b84c21d657235376beef81a2
arturfil <arturfil@hotmail.com>
2024-05-06 02:28:54 -0400

    added new functions

--- db/db.go
+++ db/db.go
@@ -2,10 +2,15 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/go-sql-driver/mysql"
 )
+
+type DB struct {
+    Client *sql.DB
+}
 
 func NewDBStorage(cfg mysql.Config) (*sql.DB, error) {
     db, err := sql.Open("mysql", cfg.FormatDSN())
@@ -14,4 +19,30 @@ func NewDBStorage(cfg mysql.Config) (*sql.DB, error) {
     }
 
     return db, nil
+}
+
+func NewDatabase(dsn string) (*DB, error) {
+    dbConn := &DB{}
+
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+    
+    err = db.Ping()
+    if err != nil {
+        return nil, err
+    }
+
+    return dbConn, nil
+}
+
+func checkDB(d *sql.DB) error {
+    err := d.Ping()
+    if err != nil {
+        fmt.Println("Error", err)
+        return err
+    }
+    fmt.Println("*** Pinged database successfully ***")
+    return nil
 }
